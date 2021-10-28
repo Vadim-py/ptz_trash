88567a1c30c45e55b1cea3738b2f5acc5ba3d7fe
Вадим Нечаев <66944880+Vadim-py@users.noreply.github.com>
2021-10-28 10:04:20 +0300

    update
    d

---
+++ open_trash/ptz_trash/maps_trash.py
@@ -0,0 +1,41 @@
+from folium import plugins
+import folium
+
+
+def map1():
+    m = folium.Map(location=[61.78491, 34.34691], zoom_start=12)
+    figure = folium.FeatureGroup(name = "Все ПЭТ мусорки")
+    m.add_child(figure)
+
+    group1 = plugins.FeatureGroupSubGroup(figure, "ПЭТ мусорки")
+    m.add_child(group1)
+
+    group2 = plugins.FeatureGroupSubGroup(figure, "Сбормобиль")
+    m.add_child(group2)
+
+    folium.Marker(
+        location = [61.76033, 34.43718],
+        popup = "ПЭТ мусорка на ключевой",
+        icon = folium.Icon(color="red", icon="trash")
+    ).add_to(group1)
+
+    folium.Marker(
+        location = [61.78848, 34.34818],
+        popup= "ПЭТ мусорка в парке 50-Летия Пионерской Организации",
+        icon=folium.Icon(color="red", icon="trash")
+    ).add_to(group1)
+
+    folium.Marker(
+        location = [61.78497, 34.36438],
+        popup= "ПЭТ мусорка в Губернаторском парке возле баскетбольной площадки",
+        icon=folium.Icon(color="red", icon="trash")
+    ).add_to(group1)
+
+    folium.Marker(
+        location = [61.78605, 34.36837],
+        popup= "ПЭТ мусорка около разводного моста",
+        icon=folium.Icon(color="red", icon="trash")
+    ).add_to(group1)
+
+    folium.LayerControl(collapsed=False).add_to(m)
+    m.save('ptz_trash/templates/1.html')    
